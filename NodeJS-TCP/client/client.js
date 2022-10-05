ece7f47fbb9bac713a44d2e5df12941f64d1ff14
root4554 <anass.kbir@hotmail.com>
2022-10-05 13:27:04 +0200

    new commit

---
+++ NodeJS-TCP/client/client.js
@@ -0,0 +1,36 @@
+const net = require("net");
+const readline = require("readline")
+const { stdin, stdout } = process;
+
+// const options = {
+//     port: 11111,
+//     host: "10.10.17.184",
+// }
+
+const options = {
+    port: process.argv[3] ?? 9000,
+    host: process.argv[2] ?? "localhost",
+}
+
+const client = net.createConnection(options);
+const rl = readline.createInterface({ input: stdin, output: stdout });
+
+client.on("connect", () => {
+    console.log("client connected");
+    runPrompt()
+});
+
+client.on("error", (err) => {
+    console.log("client error: ", err.message);
+});
+
+const runPrompt = async () => {
+    while (true) {
+        const input = await prompt()
+        client.write(input)
+    }
+}
+
+const prompt = () => new Promise(res => {
+    rl.question("> ", res)
+})
